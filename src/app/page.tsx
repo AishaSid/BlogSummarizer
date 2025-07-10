5e8c456b87fd37535004bbe29e31c84e47a9373e
AishaSid <114816803+AishaSid@users.noreply.github.com>
2025-07-10 18:31:54 +0500

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import "./global.css";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-[#efe0f9] to-[#ffffff] font-sans text-base-content">
+  <div className="container mx-auto py-2 px-2">
+   Hello 
+  </div>
+    </main>
+  );
+}
